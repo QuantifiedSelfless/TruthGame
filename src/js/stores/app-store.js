52a40c6e0cc7767e032d3824d2fabc7959a61e18
tree-waldo <leif.waldner@colorado.edu>
2016-02-25 15:11:12 -0700

    app-store

---
+++ src/js/stores/app-store.js
@@ -0,0 +1,99 @@
+'use strict';
+var React = require('react');
+var assign = require('react/lib/Object.assign');
+var EventEmitter = require('event').EventEmitter;
+
+var datablob = {
+    false_statements: ['You have used \"Your\" incorrectly on social media 74% of the time', 'You have 18 pending facebook messages you never responded to', 'You are friends with your mother on facebook', ''];
+    //replace with database call
+    true_statements: ['You have 759 friends on facebook', 'You\'ve said pasta 57 times in the last 3 years', 'You are more active on twitter after 9 pm''You\'ve commented \"Happy Birthday\" to 278 people since you started your first social media account']; 
+}
+
+//list objects for dispatcher data
+falseList = []
+trueList = []
+
+for (var i=0; i<(datablob.false_statements.length); i++) {
+    trueList.push({
+        'id': i + 1,
+        'title': datablob.true_statements[i],
+        'answer': true
+    });
+    falseList.push({
+        'id': i + 1,
+        'title': datablob.false_statements[i],
+        'answer': false
+    });
+}
+
+var PlayerList = [];
+
+function activePlayerList() {
+    return PlayerList;
+}
+
+//player object
+class player {
+    constructor(id, active) {
+        this.id = 'player' + id;
+        this.active = active;
+        this.score = 0;
+    }
+    isActive() {
+        return this.active;
+    }
+    flipActive() {
+        this.active = !this.active;
+    }
+    addScore() {
+        this.score++;
+    }
+}
+
+function activePlayer() {
+    for (var i = 0; i<PlayerList.length; i++) {
+        var temp = PlayerList[i].activePlayer();
+        if (PlayerList[i].activePlayer()) {
+            return temp;
+            break;
+        }
+    }
+}
+//appstore event emitter
+var AppStore = assign(EventEmitter.prototype, {
+    emitChange: function(change) {
+        this.emit(change);
+    },
+    addChangeListener: function(change, callback) {
+        this.on(change, callback);
+    },
+    removeChangeListener: function(change, callback) {
+        this.removeListener(change, callback);
+    },
+
+    //game specific functions
+    getFalseList: function() {
+        return falseList;
+    },
+    getTrueList: function() {
+        return trueList;
+    },
+    addPlayerScore: function() {
+       activePlayer().addScore();
+    }, 
+    //event dispatcher
+    getScore: function() {
+       return activePlayer().score;
+    },
+    //event dispatcher
+    dispatcherIndex: AppDispatcher.register(function(payload) {
+        var action = payload.action;
+        var player = activePlayer();
+        switch(action.actionType) {
+        return true; 
+        }
+    })
+})
+
+module.exports = AppStore;
+
