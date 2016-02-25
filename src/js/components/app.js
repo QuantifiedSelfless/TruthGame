71e72138850d536802212058c4e3dfcaf5a43e90
tree-waldo <leif.waldner@colorado.edu>
2016-02-25 15:07:22 -0700

    worked on app-store, added dispatcher, some player functions, player class

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -3,6 +3,15 @@ AppStore = require('../stores/app-store.js');
 AppActions = require('../actions/app-actions.js');
 
 App = React.createClass({
+
+    getInitialState: function() {   
+        return { 
+        }
+    },
+   
+    componentWillMount: function() {
+    }, 
+    
     render: {
         return (
             <h1>Hello World</h1>
