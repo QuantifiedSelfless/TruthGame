53e941f0e1a32059b8a7c874b0501c2c376a93fb
tree-waldo <leif.waldner@colorado.edu>
2016-02-24 17:43:15 -0700

    Hello World

---
+++ src/js/components/app.js
@@ -0,0 +1,13 @@
+React = require('react');
+AppStore = require('../stores/app-store.js');
+AppActions = require('../actions/app-actions.js');
+
+App = React.createClass({
+    render: {
+        return (
+            <h1>Hello World</h1>
+        )
+    }
+});
+
+module.exports = App;
---
+++ src/js/main.js
@@ -0,0 +1,4 @@
+var App = require('./components/app.js');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(<App />, document.getElementById('main'));
