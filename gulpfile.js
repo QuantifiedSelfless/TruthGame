1aa015a5b63c754d0b30d812df31426d518dd6b0
wannabeCitizen <doorsofskirpan@gmail.com>
2016-04-10 16:52:54 -0600

    gulpfile fixed; no more watcher

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ var reactify = require('reactify');
 var source = require('vinyl-source-stream');
 
 gulp.task('browserify', function() {
-    browserify('./src/js/main.js')
-        .transform('reactify')
+        browserify({
+        entries: ['./src/js/main.js'],
+        transform: [reactify]
+        })
         .bundle()
         .pipe(source('main.js'))
-        .pipe(gulp.dest('./dist/js'));
+        .pipe(gulp.dest('./dist/js/'));
 });
 
 gulp.task('copy', function() {
@@ -31,6 +33,7 @@ gulp.task('copy', function() {
         .pipe(gulp.dest('dist/assets'));
 });
 
-gulp.task('default', ['browserify', 'copy'], function() {
+gulp.task('default', ['browserify', 'copy']);
+gulp.task('watch', ['browserify', 'copy'], function() {
     return gulp.watch('src/**/*.*', ['browserify', 'copy'])
 });
